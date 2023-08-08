be299fdba532bf558ad503ed00a1458353392dc7
emmanuelofor <emmanuel.ofori@kibo.school>
2023-08-08 06:27:52 -0700

    Task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
+"""
+function that queries the Reddit API and
+prints the titles of the first 10 hot posts
+listed for a given subreddit.
+"""
 import requests
 
+
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    
-    if response.status_code == 404:
-        print("None")
+    if subreddit is None or type(subreddit) is not str:
+        print('None')
         return
-    
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+
+    base_url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    headers = {'User-Agent': '0x16-api_advanced_project'}
+    r = requests.get(base_url, headers=headers, allow_redirects=False).json()
+
+    if r.get('data') is None:
+        print('None')
+        return
+
+    i = 0
+    data = r.get('data').get('children')
+
+    for i in range(0, 10):
+        print(data[i].get('data').get('title'))
+        i += 1
